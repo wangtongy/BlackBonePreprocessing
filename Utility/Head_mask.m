491c6018486a150d8163cb2dba29a63a3adcfdc4
wangtongy <wang_t1@denison.edu>
2024-01-10 12:03:49 -0600

    change Head mask.m, comment out the threshold for levelset output

--- Utility/Head_mask.m
+++ Utility/Head_mask.m
@@ -7,10 +7,10 @@ clear levelset;
 
 nii = niftiread(name_mk);
 info = niftiinfo(name_in);
-I1 = find(nii>.5);
-I0 = find(nii<=0.5);
-nii(I1) = 1;
-nii(I0) = 0;
+% I1 = find(nii>.5);
+% I0 = find(nii<=0.5);
+% nii(I1) = 1;
+% nii(I0) = 0;
 
 mk = single(imfill_3d_3views(nii));
 
