3ebaf15d31b9d15da180fecc89596edd8f8410de
wangtongy <wang_t1@denison.edu>
2024-01-10 11:42:58 -0600

    change levelset.m main function, add output folder

--- Scripts/get_levelset_mask.m
+++ Scripts/get_levelset_mask.m
@@ -1,5 +1,5 @@
 
-function get_levelset_mask(folder, subject_list,ext)
+function get_levelset_mask(folder,out, subject_list,ext)
       % Needed to run the levelset command
     addpath('/data/anlab/TongyaoW/BlackBoneProject/Preprocessing/Matlab_NIFTI_IO/');  
     addpath(genpath('/data/anlab/TongyaoW/BlackBoneProject/Preprocessing/Utility'));
@@ -9,7 +9,7 @@ function get_levelset_mask(folder, subject_list,ext)
         tic
         fprintf('\nStarting levelset\n');
         name_in = sprintf('%s/%s_%s.nii.gz',folder,subject_list{i},ext);
-        name_mk = sprintf('%s/%s_%s_mk.nii.gz',folder,subject_list{i},ext);
+        name_mk = sprintf('%s/%s_%s_mk.nii.gz',out,subject_list{i},ext);
         if ~exist(name_in,'file')
             continue 
         end
