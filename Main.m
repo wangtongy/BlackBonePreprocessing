63b893f25d6acaa1289fa7699a4120c084c24d58
wangtongy <wang_t1@denison.edu>
2024-01-26 15:17:24 -0600

    be more organized and add inputs to function

--- Main.m
+++ Main.m
@@ -7,36 +7,33 @@ final_folder = '/data/anlab/TongyaoW/BlackBoneProject/Data/3D_Dataset/Normalized
 addpath(genpath('/data/anlab/TongyaoW/BlackBoneProject/Preprocessing/'));
 % subject_number = load_names('/data/anlab/TongyaoW/BlackBoneProject/Data/ToAndrew/Training_testing_20231204.txt');
 % subject_number_mr = load_names('/data/anlab/TongyaoW/BlackBoneProject/Data/3D_Dataset/move_header.txt');
-SB_049 = {'04'};
+subject_number = {'51','58','59','70'};
 %% Load CT dicom and generate nifti files (dicom folder structure for ct may change sometimes, In general, SB 1-63 shared one data structure, and the rest of them shared the other one,)
 % subject_number = {'056'};
-<<<<<<< HEAD
-dcm2nii(Dcm_ct_Folder,SB_049,0);
-=======
-dcm2nii(Dcm_ct_Folder,{'028'},0);
->>>>>>> origin/master
+dcm2nii(Dcm_ct_Folder,subject_number,0);
+
 %% iNTERPOLATION CT
-Interpolation(Dcm_ct_Folder,Center_folder,SB_049);
+Interpolation(Dcm_ct_Folder,Center_folder,subject_number);
 %% Generate head mask and only get the largest part (to get rid of the ct holder in the bakground)
-get_levelset_mask(Center_folder,SB_049,'nonorm_ct');
-keep_only_largest_region(Center_folder,Center_folder,SB_049,'ct');
+get_levelset_mask(Center_folder,subject_number,'nonorm_ct');
+keep_only_largest_region(Center_folder,Center_folder,subject_number,'ct');
 %% Generate ct bone mask and weighted bone mask for future MR2CT registration
 CT_Bone_mask(Center_folder,resize_subject);
 edge_filtering(Center_folder,Center_folder,subject_number,'ct');
 %% load MR Dicom and generate nifti files
 % pointer = 2; %% 1 is the rsb ct dicom; 0 is the sb ct dicom; and 2 is the rsb mr dicom;3 is the sb mr dicom;
-dcm2nii(Dcm_mr_Folder,resize_subject,1);
+dcm2nii(Dcm_mr_Folder,subject_number,1);
 %% Bias field correction
-Bias_field_correction(Dcm_mr_Folder,Center_folder,resize_subject);
+Bias_field_correction(Dcm_mr_Folder,Center_folder,subject_number);
 %% registration from mr to ct
-Registration(Center_folder,resize_subject,12,'mutualinfo'); %% 12 is the -dof number, can change if the current registration are not good;
+Registration(Center_folder,subject_number,12,'mutualinfo'); %% 12 is the -dof number, can change if the current registration are not good;
 % if results are not good, redo 
 % bad_regis = load_names('/data/anlab/TongyaoW/BlackBoneProject/Data/3D_Dataset/bad_regis.txt');
 % Registration_again(Center_folder,bad_regis,12,'corratio');
 %% generate mr head mask
-get_levelset_mask(Center_folder,resize_subject,'r1');
+get_levelset_mask(Center_folder,subject_number,'r1');
 %% get the insersection mask
 Intersection_mask(Center_folder, final_folder,subject_number);
 %% Normalize CT and MR
 normalize_images_in_dir_ct(Center_folder,final_folder,subject_number,'ct');
-normalize_images_in_dir_mr(folder,folder,resize_subject,'r1',2);
+normalize_images_in_dir_mr(Center_folder,final_folder,subject_number,'nonorm_r1','r1',2);
